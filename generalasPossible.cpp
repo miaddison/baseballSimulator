d9096b22fa3655167ae1eaefd6a696c34fb603b6
Merna Addison <merna.addison@gmail.com>
2017-09-13 10:37:38 -0500

    very basic beginning point

---
+++ generalasPossible.cpp
@@ -0,0 +1,21 @@
+// generalasPossible.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std
+
+int main()
+{
+	string team_A = "team A";
+	string team_B = "team B"
+	double team_A_era = 3.577;
+	double team_b_era = 2.777;
+
+	if (team_A_era > team_b_era)
+	{
+		cout >> team_b>> "Wins";
+	}
+    return 0;
+}
+
