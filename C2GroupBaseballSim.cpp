0c532c7999791e0bf7beee438d97c18586bbacc8
Merna Addison <merna.addison@gmail.com>
2017-09-29 14:52:05 -0500

    implementation of struct array to read in from cvs and user input validation

--- C2GroupBaseballSim.cpp
+++ C2GroupBaseballSim.cpp
@@ -1,7 +1,7 @@
 /* Advanced C++ Group project by
-    Merna Addison
+    Merna Addison   merna.addison@gmail.com
     Brian Burk
-    Ismael Ramirez
+    Ismael Ramirez1  
     James Robbins
     
     9/5/17 - 10/17/17
@@ -10,55 +10,127 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 #include <vector>
+#include <sstream>
 using namespace std;
 
-// Prototype function
-//double determineEra ();
-void determineGameWinner (double teama, double teamb);
+// Global Variable
+const int ARRAY_SIZE = 5;  // Array size
 
-//Global Variables
-vector<string>teams = {"Chicago Cubs", "Cincinnati Reds", "Milwaukee Brewers", "Pittsburgh Pirates", "St.Louis Cardinals"};    // vector with team names
-vector<double>eras = {4.08, 5.25, 4.04, 4.27, 3.86};   //vector with eras
+// Struct for team values
+struct team{
+    //string leagueName;
+    string teamName;
+    double teamEra;
+    //double pitcherEra;
+    //double player1BatAvg;
+    //double player2BatAvg;
+    //double player3BatAvg;
+}team[ARRAY_SIZE];
+
+// Prototype function
+void determineGameWinner (struct team homeTeam, struct team awayTeam);
 
 int main()
-{ 
+{
+    //Input file stream objects
+    ifstream inTeamData;
+    
+    // Variables
     int temp = 0;       //temp variable to hold team choice input to be converted to vector value
     int hometeam = 0;   //holds input for hometeam choice
     int awayteam = 0;   //holds input for awayteam choice
     int count = 0;      //count variable for selection of awayteam
+    string input;
+    string newline = "\n";
+    vector<string>tempArray;
+
     
-    //Output directions
-    cout<<"Please choose your hometeam:"<<endl;
+    // Open files
+    inTeamData.open("TeamData.csv");
     
-    //Output all teams
-    for(int x = 0; x<teams.size(); x++){
-        cout<< x+1 << ". " << teams[x] << endl;
+    if(inTeamData.is_open()){
+        while(getline(inTeamData, input)){
+            stringstream ss(input);
+            while(getline(ss, input, ',')){
+                tempArray.push_back(input);
+            }
+        }
     }
     
-    //Take in input for hometeam choice
-    cin >> temp;
+    
+    // Testing file into tempArray
+    /*for(int i = 0; i < tempArray.size(); i++){
+        cout << tempArray[i] << endl;
+    }*/
+    
+    // tempArray into Team
+    count = 0;
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        //team[i].leagueName = tempArray[count++];
+        team[i].teamName = tempArray[count++];
+        team[i].teamEra = atof(tempArray[count++].c_str());
+        //team[i].pitcherEra = atof(tempArray[count++].c_str());
+        //team[i].player1BatAvg = atof(tempArray[count++].c_str());
+        //team[i].player2BatAvg = atof(tempArray[count++].c_str());
+        //team[i].player3BatAvg = atof(tempArray[count++].c_str());
+    }
+
+    // Testing for teampArray into Team
+    /*for(int i=0; i < ARRAY_SIZE; i++){
+        cout << "Name: " << team[i].teamName << " Era: " << team[i].teamEra << endl;
+    }*/
+
+    while(temp < 1 || temp > ARRAY_SIZE){
+        //Output directions
+        cout<<"Please choose your hometeam:"<<endl;
+    
+        //Output all teams
+        for(int x = 0; x<ARRAY_SIZE; x++){
+            cout<< x+1 << ". " << team[x].teamName << endl;
+        }
+        
+        //Take in input for hometeam choice
+        cin >> temp;
+        
+        // Check input
+        if(temp < 1 || temp > ARRAY_SIZE){
+            cout << "Please choose team number 1 through " << ARRAY_SIZE << "." << endl;
+        }
+    }
     
     //Convert input to vector index
     hometeam = temp - 1;
     
     //Output choice
-    cout<< "You chose the " << teams[hometeam] << " as your hometeam."<< endl;
+    cout<< "You chose the " << team[hometeam].teamName << " as your hometeam."<< endl;
     
-	//output directions for awayteam
-    cout << "Please choose your awayteam:"<<endl;
+    //testing
+    //cout << team[hometeam].teamName << team[hometeam].teamEra <<endl;
     
-    //Output all teams skipping chosen hometeam
-    for(int x = 0; x<teams.size(); x++){
-        if(x != hometeam){
-            count++;
-            cout<< count << ". " << teams[x] << endl;
+    while(temp < 1 || temp > (ARRAY_SIZE-1)){
+        //output directions for awayteam
+        cout << "Please choose your awayteam:"<<endl;
+    
+        //Output all teams skipping chosen hometeam
+        count = 0;
+        for(int x = 0; x<ARRAY_SIZE; x++){
+            if(x != hometeam){
+                count++;
+                cout<< count << ". " << team[x].teamName << endl;
+            }
+        }
+    
+        //Take in input for awayteam choice
+        cin >> temp;
+        
+        // Check input
+        if(temp < 1 || temp > (ARRAY_SIZE-1)){
+            cout << "Please choose team number 1 through " << (ARRAY_SIZE-1)<< "." << endl;
         }
     }
-    
-    //Take in input for awayteam choice
-    cin >> temp;
     
     //Convert input to vector index
     if(temp>hometeam) {    //corrects for user number one start versus vector zero start
@@ -67,18 +139,22 @@ int main()
         awayteam = temp - 1; //corrects for user and vector start and then for the vector index skipped for hometeam
     }
 	//Output choice
-    cout<< "You chose the " << teams[awayteam] << " as your awayteam."<< endl;
+    cout<< "You chose the " << team[awayteam].teamName << " as your awayteam."<< endl;
     
-    determineGameWinner(hometeam, awayteam);
+    //testing
+    //cout << team[awayteam].teamName << team[awayteam].teamEra <<endl;
+    
+    determineGameWinner(team[hometeam], team[awayteam]);
+    
     return 0;
 }
 
 //Function to determine the winner based on era
-void determineGameWinner(double hometeam, double awayteam){
-    if (eras[hometeam] < eras[awayteam]){
-		cout << "The " << teams[hometeam]<< " win!" << endl;
+void determineGameWinner(struct team home, struct team away){
+    if (home.teamEra < away.teamEra){
+		cout << "The " << home.teamName << " win!" << endl;
 	}
 	else {
-		cout << "The " << teams[awayteam]<< " win!" << endl;
+		cout << "The " << away.teamName << " win!" << endl;
 	}
 }
