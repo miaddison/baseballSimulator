5d0d3d26d7a5622f394377d66bd92637161db78b
bburk117 <31907447+bburk117@users.noreply.github.com>
2017-10-17 06:45:41 -0500

    Add files via upload

--- C2GroupBaseballSim.cpp
+++ C2GroupBaseballSim.cpp
@@ -1,7 +1,7 @@
 /* Advanced C++ Group project by
-    Merna Addison
+    Merna Addison   merna.addison@gmail.com
     Brian Burk
-    Ismael Ramirez
+    Ismael Ramirez 
     James Robbins
     
     9/5/17 - 10/17/17
@@ -9,76 +9,257 @@
     playing against each other.
 */
 
+
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 #include <vector>
+#include <sstream>
+#include <ctime>
 using namespace std;
 
-// Prototype function
-//double determineEra ();
-void determineGameWinner (double teama, double teamb);
+// Global Variable
+const int ARRAY_SIZE = 5;  // Array size
+const int PLAYER_ARRAY = 3; // Team member batting average array
 
-//Global Variables
-vector<string>teams = {"Chicago Cubs", "Cincinnati Reds", "Milwaukee Brewers", "Pittsburgh Pirates", "St.Louis Cardinals"};    // vector with team names
-vector<double>eras = {4.08, 5.25, 4.04, 4.27, 3.86};   //vector with eras
+// Struct for team values
+struct team{
+    string teamName;
+    double teamEra;
+    double pitcherEra;
+    double playerBA [PLAYER_ARRAY];
+}team[ARRAY_SIZE];
+
+// Prototype functions
+void determineGameWinner (struct team homeTeam, struct team awayTeam, int[], int Gamesize);
+double HometeamAdv();
+bool inning (bool atBat1, bool atBat2, bool atBat3);
+bool atBat (double pitcherERA, double batterBA);
+void clearInput();
 
 int main()
-{ 
-    int temp = 0;       //temp variable to hold team choice input to be converted to vector value
-    int hometeam = 0;   //holds input for hometeam choice
-    int awayteam = 0;   //holds input for awayteam choice
-    int count = 0;      //count variable for selection of awayteam
+try
+{
+    //Input file stream objects
+    ifstream inTeamData;
     
-    //Output directions
-    cout<<"Please choose your hometeam:"<<endl;
+    // Variables
+    int teamNum = 0;       // Variable to hold team choice input to be converted to vector value
+    int hometeam = 0;       // Holds input for hometeam choice
+    int awayteam = 0;       // Holds input for awayteam choice
+    int count = 0;          // Count variable for selection of awayteam
+    string input = "";      // For taking in input
+    bool valid = false;     // Validates input for type
+    bool playAgain = false; // Allows user to play again
+    int gameSize = 9; // Holds the amount of innings in a game
+    int gameArray[gameSize]; // Array of innings representing a baseball game
     
-    //Output all teams
-    for(int x = 0; x<teams.size(); x++){
-        cout<< x+1 << ". " << teams[x] << endl;
-    }
+    // Open files
+    inTeamData.open("TeamData.csv");
     
-    //Take in input for hometeam choice
-    cin >> temp;
-    
-    //Convert input to vector index
-    hometeam = temp - 1;
-    
-    //Output choice
-    cout<< "You chose the " << teams[hometeam] << " as your hometeam."<< endl;
-    
-	//output directions for awayteam
-    cout << "Please choose your awayteam:"<<endl;
-    
-    //Output all teams skipping chosen hometeam
-    for(int x = 0; x<teams.size(); x++){
-        if(x != hometeam){
-            count++;
-            cout<< count << ". " << teams[x] << endl;
+    // Read file into struct array
+    if(inTeamData.is_open()){
+        while(inTeamData.good()){
+            for(int i = 0; i < ARRAY_SIZE; i++){
+                getline(inTeamData,input);
+                stringstream ss(input);
+                getline(ss, input, ',');
+                team[i].teamName = input;
+                getline(ss, input, ',');
+                team[i].teamEra = atof(input.c_str());
+                getline(ss, input, ',');
+                team[i].pitcherEra = atof(input.c_str());
+                for(int x = 0; x < PLAYER_ARRAY; x++){
+                    getline(ss, input, ',');
+                    team[i].playerBA[x] = atof(input.c_str());
+                }
+            }
         }
     }
     
-    //Take in input for awayteam choice
-    cin >> temp;
+    do{     // Main loop for game
+        do{     // Loop to select hometeam with user validation
+            
+            //Output directions
+            cout<<"Please choose your hometeam:"<<endl;
     
-    //Convert input to vector index
-    if(temp>hometeam) {    //corrects for user number one start versus vector zero start
-        awayteam = temp;
-    }else{
-        awayteam = temp - 1; //corrects for user and vector start and then for the vector index skipped for hometeam
-    }
-	//Output choice
-    cout<< "You chose the " << teams[awayteam] << " as your awayteam."<< endl;
+            //Output all teams
+            for(int x = 0; x<ARRAY_SIZE; x++){
+                cout<< x+1 << ". " << team[x].teamName << endl;
+            }
+        
+            // Take in input for hometeam choice and check that input is right type
+            if(cin >> teamNum){
+                cin.ignore();
+                //cout << "valid input type" << endl; //testing input type
+                // input is valid
+                valid = true;
+            
+                // Check input within correct range
+                if(teamNum < 1 || teamNum > ARRAY_SIZE){
+                    //cout << "input out of range" << endl;   // testing input range
+                    cout << "Please choose team number 1 through " << ARRAY_SIZE << "." << endl;
+                }
+            }else{
+                clearInput();
+                //cout << "input not valid" << endl; //testing input type
+                cout << "Please choose team number 1 through " << ARRAY_SIZE << "." << endl;
+            }
+        }while(teamNum < 1 || teamNum > ARRAY_SIZE || !valid); //end loop for hometeam
+        
+        // Reset valid to false so next user input can be validated for type
+        valid = false;
     
-    determineGameWinner(hometeam, awayteam);
+        //Convert input to vector index
+        hometeam = teamNum - 1;
+    
+        //Output choice
+        cout<< "You chose the " << team[hometeam].teamName << " as your hometeam."<< endl;
+    
+        //testing
+        //cout << team[hometeam].teamName << team[hometeam].teamEra <<endl;
+    
+        // Loop to determine awayteam with validation
+        do{
+            //output directions for awayteam
+            cout << "Please choose your awayteam:"<<endl;
+    
+            //Output all teams skipping chosen hometeam
+            count = 0;
+            for(int x = 0; x<ARRAY_SIZE; x++){
+                if(x != hometeam){
+                    count++;
+                    cout<< count << ". " << team[x].teamName << endl;
+                }
+            }
+    
+            // Take in input for hometeam choice and check that input is right type
+            if(cin >> teamNum){
+                cin.ignore();
+                // input is valid flag true
+                valid = true;
+                //cout << "valid input type" << endl; //testing input type
+            
+                // Check input range
+                if(teamNum < 1 || teamNum > (ARRAY_SIZE-1)){
+                    //cout << "input out of range" << endl;   //testing input range
+                    cout << "Please choose team number 1 through " << (ARRAY_SIZE-1)<< "." << endl;
+                }
+            }else{
+                clearInput();
+                //cout << "input not valid" << endl; //testing input type
+                cout << "Please choose team number 1 through " << ARRAY_SIZE << "." << endl;
+            }
+        }while(teamNum < 1 || teamNum > (ARRAY_SIZE-1) || !valid); // end loop for away team
+    
+        //Convert input to vector index
+        if(teamNum > hometeam) {    //corrects for user number one start versus vector zero start
+            awayteam = teamNum;
+        }else{
+            awayteam = teamNum - 1; //corrects for user and vector start and then for the vector index skipped for hometeam
+        }
+        //Output choice
+        cout<< "You chose the " << team[awayteam].teamName << " as your awayteam."<< endl;
+    
+        //testing
+        //cout << team[awayteam].teamName << team[awayteam].teamEra <<endl;
+    
+        // Pass home and away teams into method to determine winner based on team era
+        determineGameWinner(team[hometeam], team[awayteam], gameArray, gameSize /*, homeAdvantage*/);
+        
+        // Call Hometeam advantage for testing that output is between 0.01 and 0.10
+        //cout << HometeamAdv() << endl;
+            
+        // determine if user would like to try again and input validation
+        cout << "Would you like to play again? (y/n)" << endl;
+          
+        // take in user input on whether they'd like to play again
+        input = "";
+        cin >> input;
+        clearInput();
+        
+        // Check input either y or n prompt input again if not
+        while(input != "n" && input != "N" && input != "y" && input != "Y"){
+            //cout << "\"" << input << "\"" << endl;
+            //cout << "input not valid" << endl; //testing input
+            cout << "If you would like to play again please enter 'y'. \n" <<
+            "Otherwise enter 'n' to quit. " << endl;
+            cin >> input;
+            clearInput();
+        }
+
+        // change bool flag to answer given
+        if(input == "y" || input == "Y"){
+            playAgain = true;
+        }else{
+            playAgain = false;
+        }
+    }while(playAgain == true); // end game loop
+    
     return 0;
 }
-
+catch(exception& e){
+    cerr << e.what() << '\n';
+    system("pause");
+    return 1;
+}
+catch(...){
+    cerr << "exception \n";
+    system("pause");
+    return 2;
+}
 //Function to determine the winner based on era
-void determineGameWinner(double hometeam, double awayteam){
-    if (eras[hometeam] < eras[awayteam]){
-		cout << "The " << teams[hometeam]<< " win!" << endl;
+void determineGameWinner(struct team home, struct team away, int gameArray[], int gameSize){
+	int scoreCount = 0;
+	for (int x = 0; x < gameSize; x++){
+		gameArray[x] = inning(atBat(home.pitcherEra, away.playerBA[0]), atBat(home.pitcherEra, away.playerBA[1]), atBat(home.pitcherEra, away.playerBA[2]));
+		if (gameArray[x] == 1){
+			scoreCount++;
+		}
+	}
+	if (scoreCount > 4){
+		cout << "The home team: " << home.teamName << " wins!" << endl;
+	}
+	else{
+		cout << "The away team: " << away.teamName << " wins!" << endl;
+	}
+}
+//Fuction to determine hometeam advantage between 1-10%
+double HometeamAdv(){
+    // the code below gives the home team adv
+    srand((unsigned)time(0));
+    double Adv = rand() % 10 + 1;  // picks a random number between 1 - 10
+    Adv = Adv / 100;	// after number is picked, puts it in decimal format (0.01-0.10)
+    
+    return Adv;
+}
+
+//Function to determine winner of an inning
+bool inning (bool atBat1, bool atBat2, bool atBat3){
+	if (atBat1 + atBat2 + atBat3 > 1){
+		return 1;
+		cout << "Home team won inning" << endl;
 	}
 	else {
-		cout << "The " << teams[awayteam]<< " win!" << endl;
+		return 0;
+		cout << "Away team won inning" << endl;
 	}
+}
+
+//Function to determine winner of an at bat
+bool atBat (double pitcherERA, double batterBA){
+	if ((pitcherERA / 9) + HometeamAdv() > (batterBA)){
+		return 1;
+		cout << "Pitcher wins at bat" << endl;
+	}
+	else {
+		return 0;
+		cout << "Batter wins at bat" << endl;
+	}
+}
+
+// function to clear input
+void clearInput(){
+    cin.clear();
+    cin.ignore(80, '\n');
 }
